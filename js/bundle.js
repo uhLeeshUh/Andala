aeb01614bbd6268c7dc3f16d658e2c3fe1d74ac4
Alicia <alicia.underhill@gmail.com>
2018-05-23 17:21:41 -0400

    Allow for cells to change color on mouseover with event delegation

--- js/bundle.js
+++ js/bundle.js
@@ -103,14 +103,21 @@ document.addEventListener('DOMContentLoaded', () => {
 __webpack_require__.r(__webpack_exports__);
 class Grid {
   constructor(){
+    this.colorCell = this.colorCell.bind(this);
+  }
 
+  colorCell(e){
+    const cell = e.target;
+    cell.style.backgroundColor = 'red';
+    cell.style.border = 'none';
   }
 
   buildGrid(){
     const gridHolder = document.querySelector('.grid-holder');
+    gridHolder.addEventListener('mouseover', this.colorCell);
 
     for (let i = 0; i < 8181; i++){
-      let gridCell = document.createElement('div')
+      let gridCell = document.createElement('div');
       gridCell.className = 'grid-cell';
       gridHolder.appendChild(gridCell);
     }
