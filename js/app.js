92e1ffdb689b737d049409b73bcac5aeff4bbc97
Alicia <alicia.underhill@gmail.com>
2018-05-23 16:56:03 -0400

    Build grid

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 import Grid from './grid';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const test = document.createElement("h1");
-  test.textContent = "hellow werld";
-  const body = document.querySelector('body');
-  body.appendChild(test);
+  const grid = new Grid();
+  grid.buildGrid();
 
 });
--- js/grid.js
+++ js/grid.js
@@ -3,6 +3,16 @@ class Grid {
 
   }
 
+  buildGrid(){
+    const gridHolder = document.querySelector('.grid-holder');
+
+    for (let i = 0; i < 8181; i++){
+      let gridCell = document.createElement('div')
+      gridCell.className = 'grid-cell';
+      gridHolder.appendChild(gridCell);
+    }
+  }
+
 
 }
 
