45eea8eab1875f3603f82260e64d82ac4f686cf6
Alicia <alicia.underhill@gmail.com>
2018-05-23 17:31:57 -0400

    Make moused over cells only change color between user clicks

--- js/bundle.js
+++ js/bundle.js
@@ -103,17 +103,27 @@ document.addEventListener('DOMContentLoaded', () => {
 __webpack_require__.r(__webpack_exports__);
 class Grid {
   constructor(){
+    this.colorChangeOn = false;
     this.colorCell = this.colorCell.bind(this);
+    this.toggleColorChangeOn = this.toggleColorChangeOn.bind(this);
+  }
+
+  toggleColorChangeOn(e){
+    this.colorChangeOn =
+      this.colorChangeOn === false ? true : false;
   }
 
   colorCell(e){
-    const cell = e.target;
-    cell.style.backgroundColor = 'red';
-    cell.style.border = 'none';
+    if (this.colorChangeOn){
+      const cell = e.target;
+      cell.style.backgroundColor = 'red';
+      cell.style.border = 'none';
+    }
   }
 
   buildGrid(){
     const gridHolder = document.querySelector('.grid-holder');
+    gridHolder.addEventListener('click', this.toggleColorChangeOn);
     gridHolder.addEventListener('mouseover', this.colorCell);
 
     for (let i = 0; i < 8181; i++){
--- js/grid.js
+++ js/grid.js
@@ -1,16 +1,26 @@
 class Grid {
   constructor(){
+    this.colorChangeOn = false;
     this.colorCell = this.colorCell.bind(this);
+    this.toggleColorChangeOn = this.toggleColorChangeOn.bind(this);
+  }
+
+  toggleColorChangeOn(e){
+    this.colorChangeOn =
+      this.colorChangeOn === false ? true : false;
   }
 
   colorCell(e){
-    const cell = e.target;
-    cell.style.backgroundColor = 'red';
-    cell.style.border = 'none';
+    if (this.colorChangeOn){
+      const cell = e.target;
+      cell.style.backgroundColor = 'red';
+      cell.style.border = 'none';
+    }
   }
 
   buildGrid(){
     const gridHolder = document.querySelector('.grid-holder');
+    gridHolder.addEventListener('click', this.toggleColorChangeOn);
     gridHolder.addEventListener('mouseover', this.colorCell);
 
     for (let i = 0; i < 8181; i++){
