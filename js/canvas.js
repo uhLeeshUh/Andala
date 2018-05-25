74d578ec29a3b16a3f6acfeea04228db0622e306
Alicia <alicia.underhill@gmail.com>
2018-05-25 18:33:59 -0400

    Fix symmetry to draw correctly in all quadrants

--- js/canvas.js
+++ js/canvas.js
@@ -4,8 +4,8 @@ class Canvas {
     this.ctx = this.canvasElement.getContext('2d');
 
     this.axisPoint = [400, 325];
-    this.symDirection = 'VERTICAL';
-    this.radialOrder = 0;
+    this.symDirection = 'RADIAL';
+    this.radialOrder = 9;
 
     this.startCoordinates = [];
     this.nextCoordinates = [];
@@ -53,44 +53,106 @@ class Canvas {
 
   setCoordinates(e, startOrNext){
 
-    let firstPair, symmetricPairSet;
+    let firstPair = [e.clientX, e.clientY];
+    let symmetricPairSet;
     switch(this.symDirection){
       case 'HORIZONTAL':
-        ({ firstPair, symmetricPairSet } = this.computeAxisSymPairs(e, 'HORIZONTAL'));
+        ({ symmetricPairSet } = this.computeAxisSymPairs(e, 'HORIZONTAL'));
         break;
       case 'VERTICAL':
-        ({ firstPair, symmetricPairSet } = this.computeAxisSymPairs(e, 'VERTICAL'));
+        ({ symmetricPairSet } = this.computeAxisSymPairs(e, 'VERTICAL'));
         break;
       case 'RADIAL':
-
+        ({ symmetricPairSet } = this.computeRadialSymPairs(e));
         break;
     }
 
     if (startOrNext === 'START'){
       this.startCoordinates = [firstPair].concat(symmetricPairSet);
-      // this.startCoordinates.push(symmetricPairSet);
     } else {
       this.nextCoordinates = [firstPair].concat(symmetricPairSet);
-      // this.nextCoordinates.push(symmetricPairSet);
     }
   }
 
   computeAxisSymPairs(e, axis){
-    const firstPair = [e.clientX, e.clientY];
-    // console.log(`the clicked pair is ${firstPair}`);
     let symmetricPairX, symmetricPairY, symmetricPairSet;
     switch (axis) {
       case 'HORIZONTAL':
         symmetricPairX = e.clientX;
         symmetricPairY = ((this.axisPoint[1] - e.clientY) * 2) + e.clientY;
         symmetricPairSet = [[symmetricPairX, symmetricPairY]];
-        return { firstPair, symmetricPairSet };
+        return { symmetricPairSet };
       case 'VERTICAL':
         symmetricPairX = ((this.axisPoint[0] - e.clientX) * 2) + e.clientX;
         symmetricPairY = e.clientY;
         symmetricPairSet = [[symmetricPairX, symmetricPairY]];
-        return { firstPair, symmetricPairSet };
+        return { symmetricPairSet };
+      case 'DIAGONAL-RIGHT':
+        return;
+      case 'DIAGONAL-LEFT':
+        return;
     }
+  }
+
+  computeRadialSymPairs(e){
+    const symmetricPairSet = [];
+
+    const xDistance = (e.clientX - this.axisPoint[0]);
+    const yDistance = -(e.clientY - this.axisPoint[1]);
+    const pythagoreanSum = Math.pow(xDistance, 2) + Math.pow(yDistance, 2);
+    const radius =  Math.sqrt(pythagoreanSum);
+
+    // this.ctx.beginPath();
+    // this.ctx.arc(this.axisPoint[0], this.axisPoint[1],radius,0,2*Math.PI);
+    // this.ctx.stroke();
+    // debugger
+    let theta;
+    if (xDistance >= 0 && yDistance >= 0){
+      theta = Math.atan(yDistance / xDistance);
+    } else if (xDistance <= 0 && yDistance >= 0){
+      theta = Math.PI - Math.asin(yDistance / radius);
+    } else if (xDistance <= 0 && yDistance <= 0){
+      theta = Math.PI + Math.atan(yDistance / xDistance);
+    } else if (xDistance >= 0 && yDistance <= 0){
+      theta = (2 * Math.PI) - Math.acos(xDistance / radius);
+    }
+
+    // const theta = Math.atan(yDistance / xDistance);
+    const sliceSizeRadians = (2 * Math.PI) / this.radialOrder;
+
+    const thetaPrimes = [];
+    //note: thetaPrimes will not include theta since firstPair coordinates are
+    //      already known from the user's click
+    for (let i = 1; i <= this.radialOrder; i ++){
+      thetaPrimes.push(theta + (sliceSizeRadians * i));
+    }
+
+    console.log(theta);
+
+    thetaPrimes.forEach(angle => {
+      let canvasX;
+      let canvasY;
+      // if (angle < (Math.PI / 2)){
+        canvasX = (radius * Math.cos(angle)) + this.axisPoint[0];
+        canvasY = this.axisPoint[1] - (radius * Math.sin(angle));
+      //   console.log(`quad 1 canvasY: ${canvasY}`);
+      // } else if (angle <= Math.PI){
+      //   canvasX = (radius * Math.cos(angle)) + this.axisPoint[0];
+      //   canvasY = this.axisPoint[1] - (radius * Math.sin(angle));
+      //   console.log(`quad 2 canvasY: ${canvasY}`);
+      // } else if (angle <= 3 * Math.PI / 2){
+      //   canvasX = (radius * Math.cos(angle)) + this.axisPoint[0];
+      //   canvasY = this.axisPoint[1] - (radius * Math.sin(angle));
+      //   console.log(`quad 3 canvasY: ${canvasY}`);
+      // } else if (angle > 3 * Math.PI / 2){
+      //   canvasX = (radius * Math.cos(angle)) + this.axisPoint[0];
+      //   canvasY = this.axisPoint[1] - (radius * Math.sin(angle));
+      //   console.log(`quad 4 canvasY: ${canvasY}`);
+      // }
+
+      symmetricPairSet.push([canvasX, canvasY]);
+    });
+    return { symmetricPairSet };
   }
 
 
